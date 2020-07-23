3648b299cdb8a5a6501fbf2d3807debe8f5db8f1
Benjamin Burnette <benjamin.burnette@duke.edu>
2020-07-23 11:17:19 -0700

    added to mistakes

--- jjr_mistakes.py
+++
@@ -1,9 +0,0 @@
-
-import poem_core
-
-if __name__ == '__main__':
-
-    poem = poem_core.Poem()
-    file = open("saved_objects/jjs_mistakes.txt", "w")
-    to_write = poem.pos_to_words["JJS"]
-    file.writelines("\n".join(to_write))
