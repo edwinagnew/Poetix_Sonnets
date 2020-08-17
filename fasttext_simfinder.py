96785ef1d03a11c13cf4d81e4cc07b24859cc299
Edwin Agnew <edwin.agnew@duke.edu>
2020-08-17 17:08:48 -0400

    cached fasttext

--- fasttext_simfinder.py
+++ fasttext_simfinder.py
@@ -1,25 +1,26 @@
-import io
-from gensim.models.fasttext import FastText as FT_gensim
-from gensim.test.utils import datapath
+#import io
+#from gensim.models.fasttext import FastText as FT_gensim
+#from gensim.test.utils import datapath
 from gensim.models.keyedvectors import KeyedVectors
+import pickle
 
 
 class Sim_finder:
-    def __init__(self, model_file="wiki-news-300d-1M.vec"):
-        self.model = KeyedVectors.load_word2vec_format(model_file)
+    def __init__(self, model_file="saved_objects/fasttext/wiki-news-300d-1M.vec", pick_file="saved_objects/fasttext/model.p"):
+        try:
+            self.model = pickle.load(open(pick_file, "rb"))
+            print("loaded model from pickle")
+        except:
+            print("loading the model for the first time")
+            self.model = KeyedVectors.load_word2vec_format(model_file)
+            print("saving with pickle")
+            pickle.dump(self.model, open(pick_file, "wb"))
 
-    def load_vectors(fname):
-        fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
-        n, d = map(int, fin.readline().split())
-        data = {}
-        for line in fin:
-            tokens = line.rstrip().split(' ')
-            data[tokens[0]] = map(float, tokens[1:])
-        return data
 
     def gensim_test():
         model = KeyedVectors.load_word2vec_format("wiki-news-300d-1M.vec")
         print(model.most_similar("nights"))
 
-    def get_close_words(self, positive, negative=None):
-        return [item[0] for item in self.model.most_similar(positive, negative, topn=50)]
+    def get_close_words(self, positive, negative=None, n=50):
+        if type(positive) == str: positive = positive.split()
+        return [item[0] for item in self.model.most_similar(positive, negative, topn=n)]
