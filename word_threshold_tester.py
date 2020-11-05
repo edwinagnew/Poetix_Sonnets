0ac7692eea4d00005a55f9228377bbbd01b7a613
Edwin Agnew <edwin.agnew@duke.edu>
2020-11-04 23:25:15 -0500

    glove word selection

--- word_threshold_tester.py
+++ word_threshold_tester.py
@@ -14,9 +14,9 @@ def test_diff(diffs):
             for p in ["NN", "NNS", "ABNN"]:
                 print("theme: ", theme)
                 poem1.pos_to_words[p] = {word: s for (word, s) in poem1.pos_to_words[p].items() if
-                                        poem1.fasttext.word_similarity(word, theme.split()) > num1}
+                                         poem1.fasttext.ft_word_similarity(word, theme.split()) > num1}
                 poem2.pos_to_words[p] = {word: s for (word, s) in poem1.pos_to_words[p].items() if
-                                         poem1.fasttext.word_similarity(word, theme.split()) > num2}
+                                         poem1.fasttext.ft_word_similarity(word, theme.split()) > num2}
                 print("Words cut from %s with a threshold of %f that are kept with a threshold of %f" % (p,num2,num1))
                 print(set(poem1.pos_to_words[p]) - set(poem2.pos_to_words[p]))
                 print("length of set1 : ", len(set(poem1.pos_to_words[p])))
