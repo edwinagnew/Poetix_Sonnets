c804626794ab0487d854edb4b396d0a064949106
unknown <benburnette17@gmail.com>
2021-01-07 08:30:25 -0800

    updated story code with template architecture

--- graph_reader.py
+++ graph_reader.py
@@ -2,10 +2,13 @@ import random
 import numpy as np
 
 class Node():
-    def __init__(self, word_info=None, word_pos=None):
+    def __init__(self, word_info=None, word_pos=None, templates=None):
         self.word = word_info
         self.pos = word_pos
         self.edges = {}
+        print(templates)
+        if templates != None:
+            self.templates = [(" ".join(line.split()[:-1]), line.split()[-1]) for line in templates if "#" not in line and len(line) > 1]
 
 
 class Graph():
@@ -14,13 +17,14 @@ class Graph():
         f = open(txt_file)
         curr_line = f.readline().strip()
         self.starts = [int(num) for num in curr_line.split(',')]
-        curr_line = f.readline().strip().split(',')
+        curr_line = f.readline().strip().split('=')
         while curr_line[0]!= "":
             if curr_line[0][0] in "0123456789":
                 self.nodes[int(curr_line[0])].edges[curr_line[2]] = self.nodes[int(curr_line[1])]
             else:
-                self.nodes.append(Node(word_info=curr_line[0],word_pos=curr_line[1]))
-            curr_line = f.readline().strip().split(',')
+                curr_line = self.format_for_templates(curr_line)
+                self.nodes.append(Node(word_info=curr_line[0],word_pos=curr_line[1],templates=curr_line[2]))
+            curr_line = f.readline().strip().split('=')
         self.curr = self.nodes[random.choice(self.starts)]
 
 
@@ -34,3 +38,8 @@ class Graph():
 
     def get_verbs(self):
         return list(self.curr.edges.keys())
+
+    def format_for_templates(self, temp_list):
+        """reformats so that the templates are in an iterable format"""
+        templates = [item for item in temp_list[2:]]
+        return [temp_list[0], temp_list[1], templates]
