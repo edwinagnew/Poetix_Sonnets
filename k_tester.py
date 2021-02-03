c316b300420cb67bf104f73cb1b97bfb12ebf59f
Benjamin Burnettea <benburnette17@gmail.com>
2021-02-03 18:24:46 -0500

    fixed meter bugs, minor efficiency improvements

--- k_tester.py
+++ k_tester.py
@@ -3,8 +3,8 @@ import scenery
 
 if __name__ == "__main__":
     s = scenery.Scenery_Gen()
-    for theme in ['war', 'love', 'darkness', 'wisdom', 'death']:
-        print("Writing poem for theme '", theme, "' with k=", 10)
-        for j in range(2):
-            poem = s.write_poem_flex(theme=theme, k=10)
+    for theme in ['storms', "darkness", "war", "love"]:
+        print("Writing poem for theme '", theme, "' with k=", 3)
+        for j in range(5):
+            poem = s.write_poem_flex(theme=theme, k=3)
             print(poem)
