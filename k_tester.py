3e364d3f2771271c68c178d7336adf8bbbe64b14
Benjamin Burnettea <benburnette17@gmail.com>
2021-03-18 17:08:12 -0400

    good luck edwin

--- k_tester.py
+++ k_tester.py
@@ -1,10 +1,16 @@
 import scenery
-
+import gpt_2
 
 if __name__ == "__main__":
     s = scenery.Scenery_Gen()
-    for theme in ['storms', "darkness", "war", "love"]:
+    """
+    for theme in ['forest']:
         print("Writing poem for theme '", theme, "' with k=", 3)
         for j in range(5):
             poem = s.write_poem_flex(theme=theme, k=3)
             print(poem)
+    """
+
+    for i in range(2):
+        for theme in ["war", "forest", "wisdom", "death", "darkness"]:
+            print(s.write_poem_flex(theme=theme, k=1))
