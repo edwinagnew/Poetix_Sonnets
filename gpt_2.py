37259543eb57acfb8c80bfc4ad1afeed16c7534a
Benjamin Burnettea <benburnette17@gmail.com>
2021-03-25 17:15:52 -0400

    added passive aggressive message to gpt2

--- gpt_2.py
+++ gpt_2.py
@@ -16,7 +16,7 @@ class gpt_gen:
             # self.sonnet_words = sonnet_object.get_pos_words
             self.sonnet_object = sonnet_object
         else:
-            print("didnt give me a sonnet_method, making one myself")
+            print("didnt give me a sonnet_method, but it's fine, I don't care, I'll just make one myself")
             #self.sonnet_object = sonnet_basic.Sonnet_Gen()
             self.sonnet_object = poem_core.Poem()
             # self.sonnet_words = self.sonnet_object.get_pos_words
