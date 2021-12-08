fd1337aed146ee0fbd3c5c307600e42318443084
Lily Zhu <lily.zhu@duke.edu>
2021-12-08 14:41:29 -0500

    added params to script

--- gen_poem_script.py
+++ gen_poem_script.py
@@ -13,9 +13,12 @@ for i in range(10):
     theme = random.choice(['love', 'death', 'peace', 'war', 'forest', 'darkness', 'wisdom'])
     b = random.choice([5, 7, 10])
     k = random.choice([2, 3, 5, 7])
-    print("\n\ngenerating poem", i, theme, k, b)
+    weight_repetition = random.choice([-1, -2, -5])
+    theme_threshold = random.choice([0.5, 0.75])
+    print("\n\ngenerating poem", i, theme, k, b, weight_repetition, theme_threshold)
 
-    p = sc.write_poem_revised(theme, verbose=False, b=b, k=k, theme_lines="stanza")
+    p = sc.write_poem_revised(theme, verbose=False, b=b, k=k, theme_lines="stanza", weight_repetition=weight_repetition,
+                              theme_threshold=theme_threshold)
 
     print(p)
     poems.append(p)
