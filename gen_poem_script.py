18d92c5f4eacf1ebde6c8c883733d25e66c46965
Edwin Agnew <edwin.agnew@duke.edu>
2022-04-04 14:09:59 -0400

    generation stuff

--- gen_poem_script.py
+++ gen_poem_script.py
@@ -9,12 +9,12 @@ poems = []
 date = datetime.today().date()
 file = open("new_gen_poems/poems_" + str(date) + ".txt", "a")
 
-for i in range(5):
+for i in range(10):
     theme = random.choice(['love', 'death', 'forest', 'wisdom'])
     b = random.choice([5, 7, 9])
     k = random.choice([3, 5, 7])
     #weight_repetition = random.choice([-1])
-    weight_pen = random.choice([1.5, 2, 3])
+    weight_pen = random.choice([1.2, 1.4, 1.6, 1.8])
     print("\n\ngenerating poem", i, theme, k, b, weight_pen)
 
     p = sc.write_poem_revised(theme, verbose=False, b=b, k=k, theme_lines="stanza", rep_penalty=weight_pen)
